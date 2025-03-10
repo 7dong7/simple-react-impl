e47eef6c34109a68747a4408b41c1b3279d5d71a
7dong7 <tmdkdl777@gmail.com>
2025-03-10 21:15:04 +0900

    todoList 구현하기 (react)

--- simpleCountApp/src/App.jsx
+++ simpleCountApp/src/App.jsx
@@ -3,25 +3,6 @@ import Viewer from "./components/Viewer";
 import Controller from "./components/Controller";
 import {useState} from "react";
 
-/**
- *  section으로 구분한 이유는 컴포넌트들 마다 다른 배경을 보여주기 위함이다
- *
- *  JSX 에서 class 가 아니라 className 을 사용하는 이유는
- *      자바스크립트에서 class 라는 단어가 클래스 선언에 사용되는 예약어이기 때문이다
- *      
- *  어디서 useState 를 선언해야 되는가?
- *  Viewer, Controller, App
- *  App 에서 구현해야 한다
- *      count 는 Viewer 컴포넌트로 보낸다
- *      onClickButton 함수를 만들어서 Controller 컴포넌트로 보낸다
- *
- *  컴포넌트끼리 값을 전해주고 변경하려면 서로 부모관계야 한다.
- *      App -> Viewer
- *      App -> Controller
- *      
- *      Viewer 와 Controller 와는 관계가 없다
- *  부모에서 자식한테만 state 를 전달할 수 있다 (State Lifting)
- */
 function App() {
     const [count, setCount] = useState(0);
 
--- simpleCountApp/src/components/Controller.jsx
+++ simpleCountApp/src/components/Controller.jsx
@@ -1,38 +1,18 @@
 // 카운트 앱 컨트롤러
 const Controller = ({onClickButton}) => {
 
+    const buttonValues = [-1, -10, -100, 100, 10, 1];
+
     return (
         <div>
-            <button onClick={ () => {
-                onClickButton(-1);
-            }}>
-                -1
-            </button>
-            <button onClick={ () => {
-                onClickButton(-10);
-            }}>
-                -10
-            </button>
-            <button onClick={ () => {
-                onClickButton(-100);
-            }}>
-                -100
-            </button>
-            <button onClick={ () => {
-                onClickButton(100);
-            }}>
-                +100
-            </button>
-            <button onClick={ () => {
-                onClickButton(10);
-            }}>
-                +10
-            </button>
-            <button onClick={ () => {
-                onClickButton(1);
-            }}>
-                +1
-            </button>
+            {buttonValues.map((value) => (
+                <button
+                    key={value} // 각 버튼마다 key 를 부여
+                    onClick={() => onClickButton(value)} // 버튼 클릭시 value 를 파라미터로 전달
+                >
+                    {value > 0 ? `+${value}` : value}
+                </button>
+            ))}
         </div>
     );
 };
