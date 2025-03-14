6de6169fe4f4abaf0af6607b6389754353714c76
7dong7 <tmdkdl777@gmail.com>
2025-03-14 17:18:36 +0900

    감정 일기장 - New 페이지 UI 구현 New 페이지 기능 구현

---
+++ emotional_diary/src/components/Editor.jsx
@@ -0,0 +1,130 @@
+import "./Editor.css";
+import EmotionItem from "./EmotionItem.jsx";
+import Button from "./Button.jsx";
+
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// 감정 정보 리스트
+const emotionList = [
+    {
+        emotionId:1,
+        emotionName: "완전 좋음"
+    },
+    {
+        emotionId:2,
+        emotionName: "좋음"
+    },
+    {
+        emotionId:3,
+        emotionName: "그러저럭"
+    },
+    {
+        emotionId:4,
+        emotionName: "나쁨"
+    },
+    {
+        emotionId:5,
+        emotionName: "끔찍함"
+    }
+];
+
+// 날짜를 문자열로 변경
+const getStringedDate = (targetDate) => {
+    // 받은 날짜를 YYYY-MM-DD 형태로 만들어 준다 (09 09)
+    let year = targetDate.getFullYear();
+    let month = targetDate.getMonth()+1;
+    let date = targetDate.getDate();
+
+    if (month < 10) {
+        month = `0${month}`;
+    }
+    if (date < 10) {
+        date = `0${date}`;
+    }
+
+    return `${year}-${month}-${date}`;
+}
+
+const Editor = ({onSubmit}) => {
+
+    const [input, setInput] = useState({
+        createdDate: new Date(),
+        emotionId: 3,
+        content: ""
+    });
+    
+    // 내비게이션
+    const nav = useNavigate();
+
+    // 날짜 변경 메소드
+    const onChangeInput = (e) => {
+        // createdDate 날짜 객체이다 태그 값을 그대로 넣으려면 형변환이 필요 String -> Date
+        let name = e.target.name;
+        let value = e.target.value;
+
+        if (name === 'createdDate') {
+            value = new Date(value);
+        }
+        // 한번 처리 과정을 거치게 때문에 value 값을 바로 입력하지 않고 등록한다
+        
+        setInput({
+            ...input,
+            [name]:value
+        });
+    }
+    
+    // "작성완료" 버튼의 경우 글쓰기, 글수정에서 공통으로 사용하는 컴포넌트이기 때문에 하나의 버튼이 페이지마다 다른 기능을 가져야 한다
+    const onClickSubmitButton = () => {
+        onSubmit(input);
+    };
+    
+    return (
+        <div className={"Editor"}>
+            <section className="date_section">
+                <h4>오늘의 날짜</h4>
+                <input name="createdDate"
+                       onChange={onChangeInput}
+                       value={getStringedDate(input.createdDate)}
+                       type={"date"} />
+            </section>
+
+            <section className="emotion_section">
+                <h4>오늘의 감정</h4>
+                <div className={"emotion_list_wrapper"}>
+                    {emotionList.map((item) =>
+                        <EmotionItem
+                            key={item.emotionId}
+                            {...item}
+                            isSelected={item.emotionId === input.emotionId}
+                            onClick={()=>onChangeInput({
+                                target : {
+                                    name: "emotionId",
+                                    value: item.emotionId,
+                                }
+                            })}/>
+                    )}
+                </div>
+            </section>
+
+            <section className="content_section">
+                <h4>오늘의 일기</h4>
+                <textarea
+                    name={"content"}
+                    value={input.content}
+                    onChange={onChangeInput}
+                    placeholder={"오늘은 어땠나요?"} />
+            </section>
+
+            <section className="button_section">
+                <Button text={"취소하기"}
+                        onClick={ () => nav(-1)}/>
+                <Button
+                    onClick={onClickSubmitButton}
+                    text={"작성완료"} type={"POSITIVE"}/>
+            </section>
+        </div>
+    );
+}
+
+export default Editor;
--- emotional_diary/src/pages/New.jsx
+++ emotional_diary/src/pages/New.jsx
@@ -1,9 +1,40 @@
+import Header from "../components/Header.jsx";
+import Button from "../components/Button.jsx";
+import Editor from "../components/Editor.jsx";
+
+// 네비게이션
+import { useNavigate } from "react-router-dom";
+
+// context
+import {useContext} from "react";
+import { DiaryDispatchContext } from "../App.jsx";
 
 // 새로운 일기장을 작성하는 페이지
 const New = () => {
+    const { onCreate } = useContext(DiaryDispatchContext);
+    const nav = useNavigate()
+
+    /**
+     *  props 로 onSubmit 함수를 넘겨준다
+     *  그 이유는 컴포넌트를 재사용하는데 같은 버튼이 다른 기능을 각각 수행해야 되기 때문이다
+     *  글작성 페이지 - 작성 버튼은 새로운 글 객체를 생성해야 되고
+     *  글수정 페이지 - 작성 버튼은 기존의 글 객체의 값을 수정하는 기능을 해야되기 때문에
+     *  
+     *  기능을 editor 에서 구현하지 말고 여기서 각각 구현시키고 하위 컴포넌트에서 실행만 시킨다
+     */
+    const onSubmit = (input) => {
+        onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+        nav("/", {replace: true}) // replace 옵션을 true => "/" 경로로 이동하면서, 뒤로가기 기능을 없애버림
+    };
+
     return (
         <div>
-            New
+            <Header
+                title={"새 일기 쓰기"}
+                leftChild={<Button text={"< 뒤로 가기"}
+                onClick={() => nav(-1)}/>}
+            />
+            <Editor onSubmit={onSubmit}/>
         </div>
     );
 };
